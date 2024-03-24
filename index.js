383c51e38668f0a4848951a3bb25b68114eed779
Evelina <luna.rybnikova@gmail.com>
2024-03-24 09:53:43 +0300

    add new info in calendar.html, add js function

---
+++ index.js
@@ -0,0 +1,35 @@
+const arrayOfInputs = document.querySelectorAll(".inputTofind");
+
+arrayOfInputs.forEach((element) => {
+  element.addEventListener("click", function () {
+    inputController(element, element.checked);
+  });
+});
+
+function inputController(element, statusOfInput) {
+  if (statusOfInput === true) {
+    disableNextLink(element);
+  } else {
+    enableNextLink(element);
+  }
+}
+
+function disableNextLink(element) {
+  let nextElement = element.nextElementSibling;
+
+  while (nextElement && nextElement.tagName !== "A") {
+    nextElement = nextElement.nextElementSibling;
+  }
+
+  nextElement.setAttribute("onclick", "return false");
+}
+
+function enableNextLink(element) {
+  let nextElement = element.nextElementSibling;
+
+  while (nextElement && nextElement.tagName !== "A") {
+    nextElement = nextElement.nextElementSibling;
+  }
+
+  nextElement.removeAttribute("onclick");
+}
